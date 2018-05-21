74f23ba63f5df8f5599fcacc5e2d595a8e422cf6
Klaudia-PC\Klaudia <klaudia.budzynska@quantoo.pl>
2018-05-21 18:31:04 +0200

    Dodaj pliki projektów.

---
+++ StardestroyerBetaKLABUD/Program.cs
@@ -0,0 +1,27 @@
+﻿using StardestroyerBetaKLABUD.ServiceOldBay;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StardestroyerBetaKLABUD
+{
+    class Program
+    {
+        private static readonly OldBayKLABUDClient _oldbay = new OldBayKLABUDClient();
+
+        static void Main(string[] args)
+        {
+            _oldbay.CreateWing(new Wing()
+            {
+                Power = 200,
+                Shield = 300,
+                Name = "DeathStar"
+
+            });
+
+            Console.WriteLine(_oldbay.GetWing("DeathStar"));
+        }
+    }
+}
